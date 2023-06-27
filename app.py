9638ff7de39503408be20791d8520204ab626318
Ishu <90063059+Ishu-Yadav@users.noreply.github.com>
2023-06-27 16:31:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,5 +10,25 @@ ip = st.text_input('Enter your text :')
 op = model_nb.predict([ip])
 if st.button('PREDICT'):
   st.title(op[0])
-  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                
            
